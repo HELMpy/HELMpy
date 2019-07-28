72d4372eb1cc92f194b7c5044d0768683431b599
Bengt Lüers <bengt.lueers@gmail.com>
2019-07-28 16:52:11 +0200

    add test with matpower

--- test/test_compare_with_matpower.py
+++ test/test_compare_with_matpower.py
@@ -1,5 +1,16 @@
-from compare_several_voltages import main
+from compare_with_matpower import main
+from root_path import ROOT_PATH
 
 
-def test_compare_several_voltages():
-    main()
+def test_compare_with_matpower():
+    main(
+        # MATPOWER file name
+        matpower_xlsx_file_path=ROOT_PATH / 'Results MATPOWER case118.xlsx',
+        # HELMpy file name
+        helm_xlsx_file_path=ROOT_PATH / 'Results HELM PV1 case118 1.02 1e-08.xlsx',
+        print_all=True,
+    )
+
+
+if __name__ == '__main__':
+    test_compare_with_matpower()
