de5dc489d6df46441cb5694b503966eeab0fc207
Bengt Lüers <bengt.lueers@gmail.com>
2019-07-31 16:00:15 +0200

    extend comparison tests

--- test/test_compare_voltages.py
+++ test/test_compare_voltages.py
@@ -1,12 +1,38 @@
+import pytest
+
 from helmpy.compare.compare_voltages import main
 from helmpy.util.root_path import ROOT_PATH
 
 
-def test_compare_voltages():
-    xlsx_0_file_path = ROOT_PATH / 'data' / 'results' / \
-        'Results HELM DS M1 PV1 case118 1.02 1e-08.xlsx'
-    xlsx_1_file_path = ROOT_PATH / 'data' / 'results' / \
-        'Results HELM DS M1 PV2 case118 1.02 1e-08.xlsx'
+@pytest.mark.parametrize(
+    'xlsx_0_file_path, xlsx_1_file_path',
+    [
+        (
+            ROOT_PATH / 'data' / 'results' /
+            'Results HELM DS M1 PV1 case118 1.02 1e-08.xlsx',
+            ROOT_PATH / 'data' / 'results' /
+            'Results HELM DS M1 PV2 case118 1.02 1e-08.xlsx'
+         ),
+        (
+            ROOT_PATH / 'data' / 'results' /
+            'Results HELM DS M2 PV1 case118 1.02 1e-08.xlsx',
+            ROOT_PATH / 'data' / 'results' /
+            'Results HELM DS M2 PV2 case118 1.02 1e-08.xlsx'
+        ),
+        (
+                ROOT_PATH / 'data' / 'results' /
+                'Results HELM PV1 case118 1.02 1e-08.xlsx',
+                ROOT_PATH / 'data' / 'results' /
+                'Results HELM PV2 case118 1.02 1e-08.xlsx'
+        ),
+    ],
+    ids=(
+        'helm ds m1 pv1/pv2',
+        'helm ds m2 pv1/pv2',
+        'helm pv1/pv2',
+    )
+)
+def test_compare_voltages(xlsx_0_file_path, xlsx_1_file_path):
     main(
         xlsx_0_file_path=xlsx_0_file_path,
         xlsx_1_file_path=xlsx_1_file_path,
