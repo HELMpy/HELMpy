e6faf3a87efd3e63d238127d1be98af6258de7cf
Juan José Ortega <juanjoseop10@gmail.com>
2020-11-11 20:49:28 +0100

    delete files and scripts

--- helmpy/core/__init__.py
+++ helmpy/core/__init__.py
@@ -1,12 +1,4 @@
-from helmpy.core.helm_modified_ds_m1_pv1 import helm_modified_ds_m1_pv1
 from helmpy.core.helm import helm
 from helmpy.core.clases import create_case_data_object_from_xlsx
-
-from helmpy.core.helm_ds_m1_pv1 import helm_ds_m1_pv1
-from helmpy.core.helm_ds_m1_pv2 import helm_ds_m1_pv2
-from helmpy.core.helm_ds_m2_pv1 import helm_ds_m2_pv1
-from helmpy.core.helm_ds_m2_pv2 import helm_ds_m2_pv2
-from helmpy.core.helm_pv1 import helm_pv1
-from helmpy.core.helm_pv2 import helm_pv2
 from helmpy.core.nr import nr
 from helmpy.core.nr_ds import nr_ds
