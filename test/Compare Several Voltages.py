b4a2bdd780a17943e78d7d0a7019aef86b4a2f77
Juan José Ortega Peña <juanjoseop10@gmail.com>
2021-07-13 23:36:51 +0200

    Compare files in test folder. Need to be improved.

---
+++ test/Compare Several Voltages.py
@@ -0,0 +1,64 @@
+
+"""
+HELMpy, open source package of power flow solvers developed on Python 3 
+Copyright (C) 2019 Tulio Molina tuliojose8@gmail.com and Juan José Ortega juanjoseop10@gmail.com
+
+This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
+"""
+
+
+# Compare Several Voltages
+
+# Import libraries and functions
+import os
+import pandas as pd
+import numpy as np
+pd.set_option('display.max_rows',1000)
+pd.set_option('display.max_columns',1000)
+pd.set_option('display.width',1000)
+
+############   Modify these files names   ###################
+file_base = 'put the file name here.xlsx'     #base profile
+file_list =    ['put the file name here.xlsx',
+                'put the file name here.xlsx']
+
+print_all = False ###############################
+
+
+data = pd.read_excel(file_base,sheet_name="Buses")
+magnitude_1 = data['Voltages Magnitude']
+phase_angles_1 = data["Voltages Phase Angle"]
+
+for file in file_list:
+    data = pd.read_excel(file,sheet_name="Buses")
+    magnitude_2 = data['Voltages Magnitude']
+    phase_angles_2 = data["Voltages Phase Angle"]
+
+    M = np.zeros(len(magnitude_1))
+    P_A = np.zeros(len(magnitude_1))
+
+    if(print_all):
+        print("\n---------1---------")
+        for i in range(len(magnitude_1)):
+        	print(i,magnitude_1[i],phase_angles_1[i])
+        print("\n---------2---------")
+        for i in range(len(magnitude_1)):
+        	print(i,magnitude_2[i],phase_angles_2[i])
+
+    if(print_all):
+        print("\n----Differences between 1 and 2-----")
+    for i in range(len(magnitude_1)):
+    	M[i] = abs(magnitude_1[i]-magnitude_2[i])
+    	P_A[i] = abs(phase_angles_1[i]-phase_angles_2[i])
+    	if(print_all):
+    		print(i,"Magnitude:",M[i],"Phase Angles:",P_A[i])
+
+    print("Differences between \"%s\" y \"%s\""%(file_base,file))
+    print("Highest magnitude difference: ",np.max(M))
+    print("Highest Phase Angles difference: ",np.max(P_A))
+    print("\n\n")
+
