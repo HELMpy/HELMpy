0dc8c5c4562e391b81e1e74c585c4b42c392b69b
mvogt <mike.vogt@iee.fraunhofer.de>
2023-05-26 09:30:01 +0200

    added requirements.txt parsing to setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,9 @@ from setuptools import setup, find_packages
 with open('README.md', 'r') as readme_file:
     long_description = readme_file.read()
 
+with open('requirements.txt', 'r') as req_file:
+    requirements = req_file.readlines()
+
 TIMESTAMP = str(datetime.datetime.now().replace(microsecond=0).isoformat()).\
     replace('-', '').replace('T', '').replace(':', '')
 
@@ -13,7 +16,8 @@ setup(
     long_description=long_description,
     long_description_content_type='text/markdown',
     name='helmpy',
-    version='0.1.0' + 'rc' + TIMESTAMP,
+    # version='0.1.0' + 'rc' + TIMESTAMP,
+    version='0.1.1',
     url='https://github.com/HELMpy/HELMpy',
     description='HELMpy is an open source package of power flow solvers, including the Holomorphic Embedding Load Flow Method (HELM).',
     packages=find_packages(
@@ -24,7 +28,6 @@ setup(
         ],
     ),
     package_dir={'': '.'},
-    install_requires=[
-    ],
+    install_requires=requirements,
     zip_safe=False,
 )
